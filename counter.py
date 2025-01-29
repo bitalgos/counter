a9c928ca26fe3ad655ca5b475890cd2a685ad740
bitalgos <zurowskix@gmail.com>
2025-01-29 18:55:10 +0100

    Update counter.py
    reset counter

--- counter.py
+++ counter.py
@@ -1,6 +1,10 @@
 count=0
 print(f"Current number: {count}")
 while True:
-    input("Enter - count next number")
-    count+=1 
-    print(f"Current number: {count}")
+    user_input = input("Enter - count next number (press 'r' to restart): ")
+    if user_input.lower() == 'r':
+        count = 0  
+        print(f"Restart counter. Current number: {count}")
+    else:
+        count+=1 
+        print(f"Current number: {count}")
